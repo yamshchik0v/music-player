a4d28d6ec74a6fb53cd0c9f86a7597007983d973
Aleksandr Yamshchikov <sashayamschik0v@gmail.com>
2021-11-17 20:55:03 +0300

    changed songList.js and added some variables in script.js

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const player = document.querySelector('.player')
+const songCover = document.getElementById('player-cover')
+const songName = document.getElementById('name')
+const songArtist = document.getElementById('artist')
+
+// Buttons
---
+++ script/songList.js
@@ -0,0 +1,27 @@
+const music = [
+   {
+      name: "In And Out Of Love",
+      artist: "Armin van Buuren",
+      cover: "img-1",
+      src: "mus-1"
+   },
+   {
+      name: "With You Tonight",
+      artist: "Edgar Sound, Goat the Funky, Loe",
+      cover: "img-2",
+      src: "mus-2"
+   },
+   {
+      name: "In And Out Of Love",
+      artist: "Armin van Buuren",
+      cover: "img-3",
+      src: "mus-3"
+   },
+   {
+      name: "In And Out Of Love",
+      artist: "Armin van Buuren",
+      cover: "img-4",
+      src: "mus-4"
+   },
+
+]
